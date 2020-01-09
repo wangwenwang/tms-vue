ddbffafbe980b59fe2f012feedc8fdb884f06c1b
tangxx96 <52918538+tangxx96@users.noreply.github.com>
2020-01-09 10:06:47 +0800

    我的订单-司机端

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -6,12 +6,26 @@
   	    <div class='background'></div>
   	    <div class='background_blank'></div>
   	    <div class='head'>
-  	      <div class="image"></div>
-  	      <img  class="userinfo-avatar" src="../../assets/images/defaultHead.png">
+  	      <div class="image" >
+            
+          </div>
+          <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="imageUrl" :src="imageUrl" class="userinfo-avatar">
+              <!-- <i v-else class="el-icon-plus avatar-uploader-icon"></i> -->
+              <img  v-if="!imageUrl" class="userinfo-avatar" src="../../assets/images/defaultHead.png">
+            </el-upload> 
+          
   	      <div class='userName'>{{userName}}</div>
   	      <div class='userName'>{{cellphone}}</div>
   	    </div>
   	   	<div class="stripInfo">
+          <div  @click="toOdBid" v-if='$store.state.userInfo.userType == "driver"'><i class="iconfont icon-quanbudingdan" ></i><span>我的订单</span>
+            <i  class="iconfont icon-xiangshang" ></i></div>
   	   	  <div  @click="toMyPublish" v-if='$store.state.userInfo.userType == "driver"'><i class="iconfont icon-cheb" ></i><span>我的发布</span>
   	   	  	<i class= "iconfont icon-xiangshang" ></i></div>
   	   	  <div  @click="toHistoricalTrack"><i class="iconfont icon-lishiguiji" ></i><span>历史轨迹</span>  
@@ -41,6 +55,8 @@ import FooterIndex from "../../components/footer"
   	  	userName:"",
   	  	cellphone:"",//手机号码
   	  	VersionNum: "",//版本号
+        imageUrl: "",//用户头像
+
   	  }
   	},
   	components:{
@@ -51,6 +67,8 @@ import FooterIndex from "../../components/footer"
   	  this.cellphone = this.$store.state.userInfo.cellphone;
   	  this.VersionNum =  this.$store.state.App_Version;
   	  this.VersionNum = this.VersionNum.replace("版本:","")
+      // this.imageUrl = this.$store.state.userInfo.imageUrl;
+      
   	},
   	methods:{
   	  // 跳转到 我的订单 页面
@@ -93,6 +111,23 @@ import FooterIndex from "../../components/footer"
   	  	  }
   	    })
   	  },
+
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      },
+
   	  // 如果安卓版本低于或等于1.2.0，告诉vue
   	  compareVersion(locati) {  
   	  	var arr = locati.split('.');  
@@ -207,7 +242,32 @@ import FooterIndex from "../../components/footer"
   	  	  left: 0/50rem;
   	  	  z-index: 95;
   	  	  top: 50/50rem;
+          text-align: center;
+          .avatar-uploader .el-upload {
+            border: 3px dashed #d9d9d9;
+            border-radius: 50%;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
+
+            // margin: 0 auto 40/50rem; 
+          }
+          .avatar-uploader-icon {
+            font-size: 38/50rem;
+            color: #8c939d;
+            width: 150/50rem;
+            height: 150/50rem;
+            line-height: 100/50rem;
+            text-align: center;
+          }
+          .avatar {
+            width: 150/50rem;
+            height: 150/50rem;
+            // border-radius: 50%;
+            display: block;
+          }
   	  	}
+
   	  	.userinfo-avatar{
   	  	  width: 150/50rem;
   	  	  height: 150/50rem;
