cdc2cfae1b6d3b3965ecabd0c313e22b915e4e29
wangwenwang <372266373@qq.com>
2020-11-18 18:26:18 +0800

    提交模版文件

---
+++ src/pages/publishGoods/example.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="publishGoods">
+    <div class="container">
+		
+    </div>
+  </div>
+</template>
+<script type="text/javascript">
+  export default{
+    name:"publishGoods",
+    data(){
+      return{
+
+      }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .publishGoods{
+    overflow: hidden;
+    .container{
+      overflow: hidden;
+    }
+  }
+</style>
